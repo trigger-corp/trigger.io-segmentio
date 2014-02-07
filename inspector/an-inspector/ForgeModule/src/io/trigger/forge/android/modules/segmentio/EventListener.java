f4acb0fc84ba7bfb07b1cae3e679fe1f49e4b4fd
Antoine van Gelder <antoine@trigger.io>
2014-02-07 09:44:50 +0200

    Check that config has debug key

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/segmentio/EventListener.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/segmentio/EventListener.java
@@ -1,5 +1,7 @@
 package io.trigger.forge.android.modules.segmentio;
 
+import com.google.gson.JsonObject;
+
 import android.os.Bundle;
 import io.segment.android.Analytics;
 import io.segment.android.Options;
@@ -7,37 +9,43 @@ import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeEventListener;
 
 public class EventListener extends ForgeEventListener {
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);        
 
-        String writeKey = ForgeApp.configForPlugin("segmentio").get("android_writeKey").getAsString();
-        boolean debug = ForgeApp.configForPlugin("segmentio").get("debug").getAsBoolean() ? true : false;
-        
-        Options options = new Options();
-        options.setDebug(debug);        
-        Analytics.onCreate(ForgeApp.getActivity(), writeKey, options);
-        
-        if (debug) {
-            // reset the settings cache frequently so that as you change 
-        	// settings on your integrations page, you see the settings 
-        	// update locally ASAP.
-        	Analytics.reset(); 
-        }        
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		String writeKey = "";
+		boolean debug = false;
+
+		JsonObject config = ForgeApp.configForPlugin("segmentio");
+		writeKey = config.get("android_writeKey").getAsString();
+		if (config.has("debug")) {
+			debug = config.get("debug").getAsBoolean();
+		}
+
+		Options options = new Options();
+		options.setDebug(debug);
+		Analytics.onCreate(ForgeApp.getActivity(), writeKey, options);
+
+		if (debug) {
+			// reset the settings cache frequently so that as you change
+			// settings on your integrations page, you see the settings
+			// update locally ASAP.
+			Analytics.reset();
+		}
 	}
-	
-	
-	@Override
-    public void onStart() {
-        super.onStart();
-        Analytics.activityStart(ForgeApp.getActivity());
-    }
 
-	
-    @Override
-    public void onStop() {
-        super.onStop();
-        Analytics.activityStop(ForgeApp.getActivity());
-    }
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Analytics.activityStart(ForgeApp.getActivity());
+	}
+
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Analytics.activityStop(ForgeApp.getActivity());
+	}
 }
